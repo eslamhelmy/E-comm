3c3a54380bf3278acf6dca2b2f149e9ccc3d5aea
eslamhelmy <eslamhelmy523@gmail.com>
2017-03-10 00:31:05 +0200

    update

--- src/com/Commerce/controller/HomeServlet.java
+++ src/com/Commerce/controller/HomeServlet.java
@@ -28,6 +28,7 @@ public class HomeServlet extends HttpServlet {
     }
 
 	/**
+	 * *fdfdh
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
